d467e39c2a581f4fe67689862b87ffdaf170c2f3
Yogi Reddy <yogi8247322760@gmail.com>
2026-01-18 23:43:18 +0530

    ready for deployment

--- backend/ai_attendence/settings.py
+++ backend/ai_attendence/settings.py
@@ -71,10 +71,12 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
+    "https://attendence-anomoly-detector.vercel.app"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://attendence-anomoly-detector.vercel.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
